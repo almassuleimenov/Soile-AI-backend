b71d9f13b196bc90b334736a4e1ec17d4bf50421
aLMASIK <suleimenovalmas75@gmail.com>
2026-03-09 19:12:26 +0500

    MVP Backend is ready

--- app/database.py
+++ app/database.py
@@ -1,18 +1,18 @@
+import os
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from sqlalchemy.orm import declarative_base
 
-DATABASE_URL = (
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL", 
     "postgresql+asyncpg://postgres:superpassword123@localhost:5433/capybara_db"
 )
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 
-async_session_maker = async_sessionmaker(
-    engine, class_=AsyncSession, expire_on_commit=False
-)
+async_session_maker = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 Base = declarative_base()
-
 
 async def get_db():
     async with async_session_maker() as session:
