a0051d200619091be0e35bc7446cd193fb880ab3
aLMASIK <suleimenovalmas75@gmail.com>
2026-03-11 12:44:43 +0500

    Added yield

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ async def lifespan(app: FastAPI):
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
-    return {"message": "База данных успешно сброшена и обновлена!"}
+    yield
 
 
 app = FastAPI(title="Capybara Game API", lifespan=lifespan)
